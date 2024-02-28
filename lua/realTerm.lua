55831a9f2cae34a789d42b2c845546aa75aef084
desync <blurskye@proton.me>
2024-02-28 22:49:15 +0600

    debug commit

--- lua/realTerm.lua
+++ lua/realTerm.lua
@@ -62,7 +62,7 @@ function M.toggle_term()
             })
 
             -- Restore the mode
-            if M.term_mode == 'i' or M.term_mode == 'ic' then
+            if M.term_mode == 'i' or M.term_mode == 't' then
                 vim.defer_fn(function() vim.cmd('startinsert') end, 100)
             end
         end
